d2d898d65d864d2a68f4dbb92b77b15a62e38c1c
Zach Williams <zwilliams.dev@gmail.com>
2025-07-16 22:56:00 -0600

    Performed several refactors within the player.go file.
    Seperated resource type and amount: Made the resource type and amount seperate values

--- internal/player/player.go
+++ internal/player/player.go
@@ -3,20 +3,22 @@ package player
 type PlayerClass interface {
 	GetClass() string
 	GetStartingHealth() int
-	// Return resource type and amount
-	// Warrior: rage, mage: mana etc.
-	GetStartingResource() (string, int)
-	PerformAbility(p *Player) string
+	GetStartingResourceType() string
+	GetStartingResourceAmount() int
+	performBasicAttack(p *Player) int
+	PerformAbility(p *Player) int
 }
 
 type Player struct {
-	name       string
-	class      PlayerClass
-	level      int
-	experience int
-	health     int
-	resource   int
-	inventory  map[string]int
+	name           string
+	class          PlayerClass
+	level          int
+	experience     int
+	health         int
+	resourceType   string
+	resourceAmount int
+	// Item Name -> Quantity
+	inventory map[string]int
 }
 
 // For this small project I will create the player classes
@@ -34,8 +36,12 @@ func (w Warrior) GetStartingHealth() int {
 	return 150
 }
 
-func (w Warrior) GetStartingResource() (string, int) {
-	return "Rage", 0
+func (w Warrior) GetStartingResourceName() string {
+	return "Rage"
+}
+
+func (w Warrior) GetStartingResourceAmount() int {
+	return 0
 }
 
 // Mage
@@ -49,6 +55,42 @@ func (m Mage) GetStartingHealth() int {
 	return 100
 }
 
-func (m Mage) GetStartingResource() (string, int) {
-	return "Mana", 75
+func (m Mage) GetStartingResourceName() string {
+	return "Mana"
+}
+
+func (m Mage) GetStartingResourceAmount() int {
+	return 75
+}
+
+// Player
+func NewPlayer(name string, class PlayerClass) *Player {
+	return &Player{
+		name:           name,
+		class:          class,
+		level:          1,
+		experience:     0,
+		health:         class.GetStartingHealth(),
+		resourceType:   class.GetStartingResourceType(),
+		resourceAmount: class.GetStartingResourceAmount(),
+	}
+}
+
+func (p *Player) Name() string {
+	return p.name
+}
+
+func (p *Player) Level() int {
+	return p.Level()
+}
+
+func (p *Player) AddExperience(amount int) {
+	p.experience += amount
+	// TODO: Calculate experience required to level and handle
+	// leveling player
+}
+
+func (p *Player) AddItem(itemName string) {
+	// Increase the amount of itemName the player has
+	p.inventory[itemName]++
 }
