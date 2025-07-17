27147826d1099bf26e2a52b1e0e8469f361b994b
Zach Williams <zwilliams.dev@gmail.com>
2025-07-16 22:42:07 -0600

    player.go: Created warrior struct
    Created struct for warrior class and implemented the functions required
    by the interface.

--- internal/player/player.go
+++ internal/player/player.go
@@ -3,7 +3,7 @@ package player
 type PlayerClass interface {
 	ClassName() string
 	GetStartingHealth() int
-	GetStartingResource() int
+	GetStartingResource() (string, int)
 	PerformAbility(p *Player) string
 }
 
@@ -15,4 +15,23 @@ type Player struct {
 	health     int
 	resource   int
 	inventory  map[string]int
+}
+
+// For this small project I will create the player classes
+// in this single file in a real project I would break them
+// into individual files.
+
+// Warrior
+type Warrior struct{}
+
+func (w Warrior) ClassName() string {
+	return "Warrior"
+}
+
+func (w Warrior) GetStartingHealth() int {
+	return 150
+}
+
+func (w Warrior) GetStartingResource() (string, int) {
+	return "Rage", 0
 }
